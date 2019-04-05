5dff14cd4604fe263a9e9c1a199d3c2514c369dc
Heorhii Dubinin <heorhii_dubinin@epam.com>
2019-04-05 19:16:05 +0300

    List of cards with adding items button was implemented

--- learn_flutter/udemy_course/lib/main.dart
+++ learn_flutter/udemy_course/lib/main.dart
@@ -1,14 +1,49 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void  main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('Add Product'),
+              ),
+            ),
+            Column(
+              children: _products
+                  .map((element) =>
+                  Card(
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset('assets/food.jpeg'),
+                        Text(element)
+                      ],
+                    ),
+                  ))
+                  .toList(),
+            ),
+          ],
         ),
       ),
     );
